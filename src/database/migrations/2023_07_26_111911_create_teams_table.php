92914be7c840e296becf885a3e0623e2e1080ced
sprout90210 <sprout90210@gmail.com>
2023-07-26 11:20:37 +0900

    add teams migration

---
+++ src/database/migrations/2023_07_26_111911_create_teams_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('crest')->nullable();
+            $table->string('venue')->nullable();
+            $table->text('website')->nullable();
+            $table->text('twitter')->nullable();
+            $table->text('youtube')->nullable();
+            $table->text('instagram')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+};
