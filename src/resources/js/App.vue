9344b9852d0ed982051066150a182693b65bcc17
sprout90210 <sprout90210@gmail.com>
2023-08-05 16:04:15 +0900

    フッター作成

--- src/resources/js/App.vue
+++ src/resources/js/App.vue
@@ -2,17 +2,19 @@
   <div>
     <Header />
     <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 
 <script>
 import Header from './layouts/Header.vue';
+import Footer from './layouts/Footer.vue';
 
 export default {
   components: {
     Header,
-    
+    Footer,
   },
 };
 
---
+++ src/resources/js/layouts/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer class="w-full pt-4 flex flex-col items-center justify-center bg-gray-100 text-xs text-gray">
+        <strong class="">&copy; Football Reminder</strong>
+        <div class="p-4 flex flex-col sm:flex-row">
+            <router-link to="/terms" class="m-2 sm:m-4 text-center hover:underline">
+                利用規約
+            </router-link>
+            <router-link to="/privacy" class="m-2 sm:m-4 text-center hover:underline">
+                プライバシーポリシー
+            </router-link>
+            <router-link to="/inquiries" class="m-2 sm:m-4 text-center hover:underline">
+                お問合せ
+            </router-link>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
