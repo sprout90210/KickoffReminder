92a3fc7f4abe0c6f614ad6690a5a32fc564cb460
sprout90210 <sprout90210@gmail.com>
2023-08-09 18:36:32 +0900

    footerデザイン変更

--- src/resources/js/layouts/Footer.vue
+++ src/resources/js/layouts/Footer.vue
@@ -1,22 +1,31 @@
 <template>
-    <footer class="w-full pt-4 flex flex-col items-center justify-center bg-gray-100 text-xs text-gray">
-        <strong class="">&copy; Football Reminder</strong>
-        <div class="p-4 flex flex-col sm:flex-row">
-            <router-link to="/terms" class="m-2 sm:m-4 text-center hover:underline">
-                利用規約
-            </router-link>
-            <router-link to="/privacy" class="m-2 sm:m-4 text-center hover:underline">
-                プライバシーポリシー
-            </router-link>
-            <router-link to="/inquiry" class="m-2 sm:m-4 text-center hover:underline">
-                お問合せ
-            </router-link>
-        </div>
-    </footer>
+  <footer class="w-full flex flex-col items-center justify-center bg-gray-100 text-xs">
+    <div class="pt-3 flex flex-col sm:flex-row text-gray-500">
+      <router-link
+        to="/terms"
+        class="m-2 sm:m-4 text-center hover:underline hover:text-gray-800"
+      >
+        利用規約
+      </router-link>
+      <router-link
+        to="/privacy"
+        class="m-2 sm:m-4 text-center hover:underline hover:text-gray-800"
+      >
+        プライバシーポリシー
+      </router-link>
+      <router-link
+        to="/inquiry"
+        class="m-2 sm:m-4 text-center hover:underline hover:text-gray-800"
+      >
+        お問合せ
+      </router-link>
+    </div>
+    <strong class="p-6 text-gray-700">&copy; Football Reminder</strong>
+  </footer>
 </template>
 
 <script>
 export default {
-    name: 'Footer',
-}
+  name: "Footer",
+};
 </script>
