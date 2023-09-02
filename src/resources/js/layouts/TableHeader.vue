40bd3ea63eecbba292deb1d6153c42463c26c580
sprout90210 <sprout90210@gmail.com>
2023-09-02 21:35:47 +0900

    TableHeader.vue作成

---
+++ src/resources/js/layouts/TableHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <thead>
+    <tr class="text-gray-500 text-xs italic bg-zinc-100 h-8 border-gray border-b-2">
+      <th class="w-9 sm:w-12"></th>
+      <th class="w-6 sm:w-12"></th>
+      <th class="pl-3 sm:pl-12 text-left sm:w-2/5">チーム</th>
+      <th class="hidden sm:table-cell w-8">試合</th>
+      <th class="w-6 sm:w-8">勝</th>
+      <th class="w-6 sm:w-8">分</th>
+      <th class="w-6 sm:w-8">負</th>
+      <th class="hidden sm:table-cell sm:w-8"><abbr title="総得点数">得</abbr></th>
+      <th class="hidden sm:table-cell sm:w-8"><abbr title="総失点数">失</abbr></th>
+      <th class="hidden sm:table-cell sm:w-8"><abbr title="得失点差">差</abbr></th>
+      <th class="w-8 sm:w-12">Pts</th>
+    </tr>
+  </thead>
+</template>
