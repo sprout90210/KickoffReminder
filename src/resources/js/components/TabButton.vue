d85099b63e3fad4941b6a21b95c3147d641f9d47
sprout90210 <sprout90210@gmail.com>
2023-09-02 21:32:14 +0900

    TabButton.vue作成

---
+++ src/resources/js/components/TabButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    class="px-4 sm:px-8 py-1.5 sm:py-2 ml-3 sm:ml-5 rounded-t text-sm duration-300"
+    :class="{
+      'bg-white font-bold hover:cursor-default': isActive,
+      'bg-gray-300 hover:bg-fuchsia-400 hover:text-white': !isActive,
+    }"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+import { computed, defineProps } from "vue";
+
+const props = defineProps({
+  activeTab: String,
+  tabName: String,
+});
+
+const isActive = computed(() => props.activeTab === props.tabName);
+</script>
