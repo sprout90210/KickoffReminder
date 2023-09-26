0d9c39a3164172fa4bb13cf5198c0bb0707570c6
uneasy90210 <uneasy90210@gmail.com>
2023-09-26 21:41:16 +0900

    TeamHero.vue 追加

---
+++ src/resources/js/components/TeamHero.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="relative pt-5 pb-12 sm:pb-16 bg-gradient-to-tr from-gray-300 via-gray-50 to-white">
+    <div class="flex flex-col sm:flex-row justify-center items-center">
+      <div class="p-2 sm:pl-16">
+        <img
+          :src="generateImgUrlDev(team.crest)"
+          alt="crest"
+          class="w-24 h-24 sm:h-32 sm:w-32"
+        />
+      </div>
+      <div class="flex flex-col items-center sm:mx-10">
+        <h1 class="my-2 sm:my-6 sm:text-xl font-bold">{{ team.name }}</h1>
+        <div class="m-2 flex text-xs items-center" v-if="team.venue">
+          <img
+            :src="generateImgUrlDev('stadium.png')"
+            alt="stadium"
+            class="h-6 w-5 mr-2"
+          />
+          <div>
+            {{ team.venue }}
+          </div>
+        </div>
+        <div class="flex h-8 m-2">
+          <a
+            v-if="team.youtube_url"
+            :href="team.youtube_url"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="block w-8 h-8 mx-2 rounded-full"
+          >
+            <img
+              :src="generateImgUrlDev('youtube.jpeg')"
+              alt="youtube"
+              class="rounded-full"
+            />
+          </a>
+          <a
+            v-if="team.twitter_url"
+            :href="team.twitter_url"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="block w-8 h-8 mx-2 rounded-full"
+          >
+            <img :src="generateImgUrlDev('x.jpeg')" alt="twitter" class="rounded-full" />
+          </a>
+          <a
+            v-if="team.instagram_url"
+            :href="team.instagram_url"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="block w-8 h-8 mx-2 rounded-full"
+          >
+            <img
+              :src="generateImgUrlDev('instagram.png')"
+              alt="instagram"
+              class="rounded-full"
+            />
+          </a>
+        </div>
+        <div class="text-xs sm:text-sm my-4" v-if="team.website_url">
+          <span> Website: </span>
+          <a
+            :href="team.website_url"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-sky-500 underline"
+          >
+            {{ team.website_url }}
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="flex absolute bottom-0 z-2 left-0 pl-4 sm:pl-12">
+      <TabButton
+        :activeTab="activeTab"
+        tabName="standings"
+        @click="tabChange('standings')"
+      >
+        順位
+      </TabButton>
+      <TabButton :activeTab="activeTab" tabName="results" @click="tabChange('results')" >
+        結果
+      </TabButton>
+      <TabButton :activeTab="activeTab" tabName="schedule" @click="tabChange('schedule')" >
+        予定
+      </TabButton>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import TabButton from "./TabButton.vue";
+
+const props = defineProps({
+  team: {
+    type: Object,
+    required: true,
+  },
+  activeTab: {
+    type: String,
+    required: true,
+  }
+});
+
+const emit = defineEmits(["tabChange"]);
+const tabChange = (tabName) => {
+  emit('tabChange', tabName);
+};
+
+const generateImgUrlDev = (ImgName) => {
+  const ImgUrl = "/images/" + ImgName;
+  return ImgUrl;
+};
+</script>
