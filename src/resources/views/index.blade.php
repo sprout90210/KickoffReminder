5f98dc38c7a4089ac0f321956626f493155f07f4
sprout90210 <sprout90210@gmail.com>
2023-12-05 18:48:18 +0900

    アプリケーションネーム変更 #35

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Football Reminder</title>
+    <title>Kickoff Reminder</title>
     @vite('resources/css/app.css')
 </head>
 
