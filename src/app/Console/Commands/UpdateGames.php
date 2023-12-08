84c8659033ec0c82f386bd127a8e5bf71b154642
sprout90210 <sprout90210@gmail.com>
2023-12-08 21:21:12 +0900

    standingsとgamesを更新するcommand追加

---
+++ src/app/Console/Commands/UpdateGames.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use GuzzleHttp\Client;
+use App\Models\Game;
+use Carbon\Carbon;
+
+class UpdateGames extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:updateGames';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update games from external API';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $token = config('api.X_AUTH_TOKEN');
+        $client = new Client([
+            'base_uri' => 'http://api.football-data.org/v4/',
+            'headers' => [
+                'X-Auth-Token' => $token,
+            ],
+        ]);
+
+        $competition_ids = [2014, 2021, 2015, 2002, 2019, 2001];
+
+        foreach ($competition_ids as $competition_id) {
+            try {
+                $res = $client->get("competitions/{$competition_id}/matches");
+                $games = json_decode($res->getBody())->matches;
+                $bulkData = [];
+                foreach ($games as $game) {
+                    $bulkData[] = [
+                        'id' => $game->id,
+                        'competition_id' => $game->competition->id,
+                        'season_id' => $game->season->id,
+                        'home_team_id' => $game->homeTeam->id,
+                        'away_team_id' => $game->awayTeam->id,
+                        'home_team_score' => $game->score->fullTime->home,
+                        'away_team_score' => $game->score->fullTime->away,
+                        'status' => $game->status,
+                        'stage' => $game->stage,
+                        'group' => $game->group,
+                        'utc_date' => Carbon::parse($game->utcDate)->format('Y-m-d H:i:s'),
+                        'last_updated' => Carbon::parse($game->lastUpdated)->format('Y-m-d H:i:s'),
+                    ];
+                }
+                Game::upsert($bulkData, ['id'], ['competition_id', 'season_id', 'home_team_id', 'away_team_id', 'home_team_score', 'away_team_score', 'status', 'stage', 'group', 'utc_date', 'last_updated']);
+
+            } catch (\GuzzleHttp\Exception\GuzzleException $e) {
+                $this->error('リクエストに失敗しました: ' . $e->getMessage());
+            }
+        }
+    }
+}
---
+++ src/app/Console/Commands/UpdateStandings.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use GuzzleHttp\Client;
+use App\Models\Standing;
+
+class UpdateStandings extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:updateStandings';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update standings from external API';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $token = config('api.X_AUTH_TOKEN');
+        $client = new Client([
+            'base_uri' => 'http://api.football-data.org/v4/',
+            'headers' => [
+                'X-Auth-Token' => $token,
+            ],
+        ]);
+
+        $competition_ids = [2014, 2021, 2015, 2002, 2019];
+
+        foreach ($competition_ids as $competition_id) {
+            try {
+                $res = $client->get("competitions/{$competition_id}/standings");
+                $res_json = json_decode($res->getBody());
+                $standings = $res_json->standings[0]->table;
+                $bulkData = [];
+                foreach ($standings as $standing) {
+                    $bulkData[] = [
+                        'season_id' => $res_json->season->id,
+                        'position' => $standing->position,
+                        'competition_id' => $competition_id,
+                        'team_id' => $standing->team->id,
+                        'played_games' => $standing->playedGames,
+                        'won' => $standing->won,
+                        'draw' => $standing->draw,
+                        'lost' => $standing->lost,
+                        'goals_for' => $standing->goalsFor,
+                        'goals_against' => $standing->goalsAgainst,
+                        'goal_difference' => $standing->goalDifference,
+                        'points' => $standing->points,
+                    ];
+                }
+                Standing::upsert($bulkData, ['season_id', 'team_id'], ['competition_id', 'position', 'played_games', 'won', 'draw', 'lost', 'goals_for', 'goals_against', 'goal_difference', 'points']);
+
+            } catch (\GuzzleHttp\Exception\GuzzleException $e) {
+                $this->error('リクエストに失敗しました: ' . $e->getMessage());
+            }
+        }
+    }
+}
