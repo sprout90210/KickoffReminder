083f478febabf967ee49e34a241e5496cd975c66
sprout90210 <sprout90210@gmail.com>
2023-12-17 08:57:25 +0900

    resultsとschedulesをgamesに統合

---
+++ src/resources/js/components/Games.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="games.length" class="p-1 sm:py-10 w-full max-w-3xl">
+    <p class="text-right text-gray-400 text-xxs w-full border-b pr-3">※日本時間</p>
+    <Game v-for="game in games" :key="game.id" :game="game" class="border-b" />
+  </div>
+  <NotFound v-else />
+</template>
+
+<script setup>
+import Game from "./Game.vue";
+import NotFound from "./NotFound.vue";
+ 
+const props = defineProps({
+  games: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
