379c77372119ca82c7c27960d898569afb10d489
sprout90210 <sprout90210@gmail.com>
2024-02-16 21:11:26 +0900

    pending_usersテーブル追加

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique()->nullable();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->string('line_user_id')->unique()->nullable();
             $table->unsignedInteger('remind_time')->default(15);
