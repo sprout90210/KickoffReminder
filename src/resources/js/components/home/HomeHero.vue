043eafdb92170c5ee066925b92495199db170960
sprout90210 <sprout90210@gmail.com>
2024-03-22 10:54:23 +0900

    home更新

--- src/resources/js/components/home/HomeHero.vue
+++ src/resources/js/components/home/HomeHero.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="bg-home-1">
-    <h1 class="text-5xl sm:text-8xl font-black">Kickoff Reminder</h1>
-    <div class="text-lg sm:text-xl my-12 sm:mt-24 flex flex-col">
-      <p>サッカーの試合を常に把握しましょう。</p>
-      <p>簡単な登録だけで、好きなチームの試合通知が受け取れます。</p>
+    <h1 class="text-6xl md:text-8xl font-black flex flex-col sm:flex-row items-start">
+      <p class="sm:mr-10">Kickoff</p>
+      <p>Reminder</p>
+    </h1>
+    <p class="subtitle text-gray-200 text-sm sm:text-base my-2">サッカー専門の試合通知サービス</p>
+    <div v-if="!isLoggedIn" class="flex flex-wrap items-center justify-center sm:my-8">
+      <router-link :to="{ name: 'EmailVerificationRequest' }" class="registration-link m-2 sm:m-5">新規登録</router-link>
+      <router-link :to="{ name: 'Login' }" class="login-link m-2 sm:m-5">ログイン</router-link>
+      <button @click="loginLine" class="line-login-link m-2 sm:m-5">LINEログイン</button>
     </div>
-    <div v-if="!isLoggedIn" class="flex flex-wrap items-center justify-center">
-      <router-link :to="{ name: 'EmailVerificationRequest' }" class="registration-link m-3 sm:m-3">新規登録</router-link>
-      <router-link :to="{ name: 'Login' }" class="login-link m-3 sm:m-3">ログイン</router-link>
-      <button @click="loginLine" class="line-login-link m-2 sm:m-3">LINEログイン</button>
+    <div class="text-sm sm:text-lg flex flex-col sm:flex-row items-start font-bold" :class="isLoggedIn ? 'my-20' : 'my-6'">
+      <p class="mb-2">お気に入り登録一つで、</p>
+      <p>あなたが応援するチームの試合時刻をお知らせします。</p>
     </div>
   </div>
 </template>
